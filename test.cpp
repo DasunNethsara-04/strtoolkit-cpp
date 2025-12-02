0f943fc5dd381a977166231f356f90a61e192717
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-12-02 20:01:56 +0530

    testing sample code added

--- test.cpp
+++ test.cpp
@@ -2,8 +2,18 @@
 #include "strtoolkit.h"
 
 int main() {
-	const char text[] = "Hello";
-	std::cout << StrLen(text) << std::endl;
+	const char text1[] = "Hello";
+
+	// testing the StrLen() function with both C-styles strings and std::string
+	std::cout << StrLen(text1) << std::endl;
+
+	const char text2[] = "Hello";
+	const std::string text3 = "Hello!";
+	const std::string text4 = "Hell!";
+	
+	// testing the StrCmp() function with both C-styles strings and std::string
+	std::cout << StrCmp(text1, text2) << std::endl;
+	std::cout << StrCmp(text3, text4) << std::endl;
 
 	return 0;
 }
