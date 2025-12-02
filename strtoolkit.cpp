da796ea1b7a7682169233a7fef361965301a9acd
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-12-02 19:55:33 +0530

    Implement StrLen and StrCmp functions for std::string and C-style strings

--- strtoolkit.cpp
+++ strtoolkit.cpp
@@ -1,17 +1,63 @@
 #include "strtoolkit.h"
 
+/**
+ * Implementation of StrLen for C-style strings.
+ * Uses a pointer to traverse the string until '\0'.
+ */
 size_t StrLen(const char* str) {
 	const char* ptr = str;
 	size_t len = 0;
-	
-	while (*ptr != '\0') {
-		len++;
-		ptr++;
-	}
 
+	while (*ptr != '\0') {
+		++len;
+		++ptr;
+	}
 	return len;
 }
 
+/**
+ * Implementation of StrLen for std::string.
+ * Uses c_str() to access the internal buffer and pointer traversal.
+ */
 size_t StrLen(const std::string& str) {
-	return 0;
+	size_t len = 0;
+	const char* ptr = str.c_str();
+
+	while (*ptr != '\0') {
+		++len;
+		++ptr;
+	}
+	return len;
+}
+
+/**
+ * Implementation of StrCmp for C-style strings.
+ * Compares character by character until both reach '\0'.
+ */
+bool StrCmp(const char* str1, const char* str2) {
+	const char* str1ptr = str1;
+	const char* str2ptr = str2;
+
+	while(*str1ptr != '\0' && *str2ptr != '\0') {
+		if (*str1ptr != *str2ptr) return false;
+		str1ptr++;
+		str2ptr++;
+	}
+	return *str1ptr == '\0' && *str2ptr == '\0';
+}
+
+/**
+ * Implementation of StrCmp for std::string.
+ * Uses c_str() to access internal buffer and compares via pointers.
+*/
+bool StrCmp(const std::string& str1, const std::string& str2) {
+	const char* str1ptr = str1.c_str();
+	const char* str2ptr = str2.c_str();
+
+	while (*str1ptr != '\0' && *str2ptr != '\0') {
+		if (*str1ptr != *str2ptr) return false;
+		str1ptr++;
+		str2ptr++;
+	}
+	return *str1ptr == '\0' && *str2ptr == '\0';
 }
