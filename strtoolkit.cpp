72676de4ac5f0099cd19800e44da8b2e28e8f163
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-12-08 16:17:10 +0530

    ToLower, StrNCpy, StrNCat, StrNCmp functions implemented

--- strtoolkit.cpp
+++ strtoolkit.cpp
@@ -17,21 +17,6 @@ size_t StrLen(const char* str) {
 }
 
 /**
- * Implementation of StrLen for std::string.
- * Uses c_str() to access the internal buffer and pointer traversal.
- */
-size_t StrLen(const std::string& str) {
-	size_t len = 0;
-	const char* ptr = str.c_str();
-
-	while (*ptr != '\0') {
-		++len;
-		++ptr;
-	}
-	return len;
-}
-
-/**
  * Implementation of StrCmp for C-style strings.
  * Compares character by character until both reach '\0'.
  */
@@ -40,22 +25,6 @@ bool StrCmp(const char* str1, const char* str2) {
 	const char* str2ptr = str2;
 
 	while(*str1ptr != '\0' && *str2ptr != '\0') {
-		if (*str1ptr != *str2ptr) return false;
-		++str1ptr;
-		++str2ptr;
-	}
-	return *str1ptr == '\0' && *str2ptr == '\0';
-}
-
-/**
- * Implementation of StrCmp for std::string.
- * Uses c_str() to access internal buffer and compares via pointers.
-*/
-bool StrCmp(const std::string& str1, const std::string& str2) {
-	const char* str1ptr = str1.c_str();
-	const char* str2ptr = str2.c_str();
-
-	while (*str1ptr != '\0' && *str2ptr != '\0') {
 		if (*str1ptr != *str2ptr) return false;
 		++str1ptr;
 		++str2ptr;
@@ -76,21 +45,6 @@ void StrCpy(const char* src, char* dest) {
 	*ptr2 = '\0';
 }
 
-void StrCpy(const std::string& src, std::string& dest) {
-	// this thing can easily perform with dest = src.
-	// This is just a code to practice and test my knowledge about pointers
-	dest.resize(src.size());
-	const char* ptr1 = src.c_str();
-	char* ptr2 = &dest[0];
-
-	while (*ptr1 != '\0')
-	{
-		*ptr2 = *ptr1;
-		++ptr1;
-		++ptr2;
-	}
-}
-
 void StrCat(const char* src, char* dest) {
 	const char* src_ptr = src;
 	char* dest_ptr = dest;
@@ -102,10 +56,6 @@ void StrCat(const char* src, char* dest) {
 		++dest_ptr;
 	}
 	*dest_ptr = '\0';
-}
-
-void StrCat(const std::string& src, std::string& dest) {
-	dest += src;
 }
 
 const char* StrChr(const char* str, int ch) {
@@ -138,6 +88,10 @@ char get_upper(char c) {
 	return std::toupper(static_cast<unsigned char>(c));
 }
 
+char get_lower(char c) {
+	return std::tolower(static_cast<unsigned char>(c));
+}
+
 void ToUpper(char* str) {
 	char* ptr = str;
 	while (*ptr != '\0') {
@@ -146,11 +100,50 @@ void ToUpper(char* str) {
 	}
 }
 
-void ToUpper(std::string& str) {
-	if (str.empty()) return;
-	char* ptr = &str[0];
+void ToLower(char* str) {
+	char* ptr = str;
 	while (*ptr != '\0') {
-		*ptr = get_upper(*ptr);
+		*ptr = get_lower(*ptr);
 		++ptr;
 	}
 }
+
+void StrNCpy(char *dest, const char *src, const size_t n) {
+    char *ptr1 = dest;
+    const char *ptr2 = src;
+    size_t i = 0;
+	while (i < n && *ptr2 != '\0') {
+		*ptr1 = *ptr2;
+		++ptr1;
+		++ptr2;
+		++i;
+	}
+    *ptr1 = '\0';
+}
+
+void StrNCat(char* dest, const char* src, const size_t n) {
+	char* ptr1 = dest;
+	const char* ptr2 = src;
+	size_t i = 0;
+	while (*ptr1 != '\0') ptr1++;
+
+	while (i < n && *ptr2 != '\0') {
+		*ptr1 = *ptr2;
+		++ptr1;
+		++ptr2;
+		++i;
+	}
+	*ptr1 = '\0';
+}
+
+bool StrNCmp(const char* str1, const char* str2, size_t n) {
+	while (n--) {
+		if (*str1 != *str2) return false;
+		if (*str1 == '\0') return true;
+		++str1;
+		++str2;
+	}
+	return true;
+}
+
+
